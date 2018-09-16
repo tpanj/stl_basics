8104dd71fd93d969440f8e60e7bb761716bcde37
Tadej Panjtar <tadej.panjtar@gmail.com>
2018-09-16 22:14:46 +0200

    retab

--- big_vector.cc
+++ big_vector.cc
@@ -6,15 +6,15 @@ using namespace std;
 int main() {
 	int i;
 	vector<int> v1;
-    v1.reserve(N); // speed up
+	v1.reserve(N); // speed up
 	for (i = 0; i < N; ++i)
 		v1.push_back(i);
 
-    cout << v1.size() << endl;
-    cout << v1.back() << endl;
-    v1.pop_back();
-    cin >> i;
-    cout << v1.size() << endl;
+	cout << v1.size() << endl;
+	cout << v1.back() << endl;
+	v1.pop_back();
+	cout << v1.size() << endl;
+	cin >> i;
 	return 0;
 }
 
